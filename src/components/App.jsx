2c43f57a3e7e060884ba4dca9aadefe26982e2af
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2024-07-21 20:37:03 +0300

    add task3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import Profile from './profile/Profile';
 import userData from '../userData.json';
 import friends from '../friends.json';
 import FriendList from './friendList/FriendList';
+import transactions from '../transactions.json';
+import TransactionHistory from './transactionHistory/TransactionHistory';
 
 const App = () => {
   return (
@@ -15,6 +17,7 @@ const App = () => {
       />
 
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
